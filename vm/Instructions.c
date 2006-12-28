e1d19ec871c612cca74b9253ba03ea8917f1c4ea
Laurence Tratt <laurie@tratt.net>
2006-12-28 19:32:00 +0000

    Move instruction names into their own file.
    This is so that more than one .c file can #include Instructions.h.

--- vm/Instructions.c
+++ vm/Instructions.c
@@ -19,21 +19,11 @@
 // IN THE SOFTWARE.
 
 
-#ifndef _CON_INSTRUCTIONS_H
-#define _CON_INSTRUCTIONS_H
+#include "Config.h"
 
-extern const char *Con_Instructions_names[52];
+#include "Instructions.h"
 
-#if (SIZEOF_CON_INT == 4)
 
-#include "Platform/Instructions/32bit.h"
 
-#elif (SIZEOF_CON_INT == 8)
 
-#include "Platform/Instructions/64bit.h"
-
-#else
-#error "Unknown word size"
-#endif
-
-#endif
+const char *Con_Instructions_names[52] = {"**unused**", "ASSIGN", "VAR_LOOKUP", "VAR_ASSIGN", "INT", "ADD_FAILURE_FRAME", "ADD_FAIL_UP_FRAME", "REMOVE_FAILURE_FRAME", "IS_ASSIGNED", "IS", "FAIL_NOW", "POP", "LIST", "SLOT_LOOKUP", "APPLY", "FUNC_DEF", "RETURN", "BRANCH", "YIELD", "**unused**", "IMPORT", "DICT", "DUP", "PULL", "CHANGE_FAIL_POINT", "STRING", "BUILTIN_LOOKUP", "ASSIGN_SLOT", "EYIELD", "ADD_EXCEPTION_FRAME", "**unused**", "INSTANCE_OF", "REMOVE_EXCEPTION_FRAME", "RAISE", "SET_ITEM", "UNPACK_ARGS", "SET", "BRANCH_IF_NOT_FAIL", "BRANCH_IF_FAIL", "CONSTANT_GET", "CONSTANT_SET", "PRE_SLOT_LOOKUP_APPLY", "UNPACK_ASSIGN", "EQ", "LE", "ADD", "SUBTRACT", "NEQ", "LE_EQ", "GR_EQ", "GT", "MODULE_LOOKUP"};

20fb9a13a91133f749aa72ab115c2d96875474ce
Laurence Tratt <laurie@tratt.net>
2007-02-13 11:42:00 +0000

    Use platform independent fgetln.
    I stupidly used the OpenBSD fgetln without checking to see whether it would even compile on another OS. Mea culpa.

--- vm/Platform/fgetln.c
+++ vm/Platform/fgetln.c
@@ -1,140 +1,75 @@
-/*	$OpenBSD: fgetln.c,v 1.7 2005/08/08 08:05:36 espie Exp $ */
-/*-
- * Copyright (c) 1990, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * Chris Torek.
+/*		$OpenPackages: util.c,v 1.10 2001/07/22 01:00:52 harlan Exp $ */
+/*		$OpenBSD: util.c,v 1.16 2001/05/23 12:34:51 espie Exp $		*/
+/*		$NetBSD: util.c,v 1.10 1996/12/31 17:56:04 christos Exp $		*/
+/* $TenDRA: contrib/make/util.c,v 1.5 2003/02/11 21:16:16 asmodai Exp $ */
+
+/*
+ * Copyright (c) 2001 Marc Espie.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *	notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ *	notice, this list of conditions and the following disclaimer in the
+ *	documentation and/or other materials provided with the distribution.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE OPENBSD PROJECT AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OPENBSD
+ * PROJECT OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "local.h"
-
 /*
- * Expand the line buffer.  Return -1 on error.
+ * Missing stuff from OS's
  */
-int
-__slbexpand(FILE *fp, size_t newsize)
-{
-	void *p;
 
-	if (fp->_lb._size >= newsize)
-		return (0);
-	if ((p = realloc(fp->_lb._base, newsize)) == NULL)
-		return (-1);
-	fp->_lb._base = p;
-	fp->_lb._size = newsize;
-	return (0);
+#include <sys/param.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Config.h"
+
+
+static void removeCR(char *s)
+{
+	while (*s) {
+		if (*s == '\r' && s[1] == '\n') {
+			*s = '\n';
+			s[1] = 0;
+			break;
+		}
+		s++;
+	}
 }
 
-/*
- * Get an input line.  The returned pointer often (but not always)
- * points into a stdio buffer.  Fgetline does not alter the text of
- * the returned line (which is thus not a C string because it will
- * not necessarily end with '\0'), but does allow callers to modify
- * it if they wish.  Thus, we set __SMOD in case the caller does.
- */
-char *
-fgetln(FILE *fp, size_t *lenp)
+char *fgetln(FILE *stream, size_t *len)
 {
-	unsigned char *p;
-	size_t len;
-	size_t off;
+	static char *buffer = NULL;
+	static size_t buflen = 0;
 
-	/* make sure there is input */
-	if (fp->_r <= 0 && __srefill(fp)) {
-		*lenp = 0;
-		return (NULL);
+	if (buflen == 0) {
+		buflen = 512;
+		buffer = malloc(buflen+1);
 	}
-
-	/* look for a newline in the input */
-	if ((p = memchr((void *)fp->_p, '\n', fp->_r)) != NULL) {
-		char *ret;
-
-		/*
-		 * Found one.  Flag buffer as modified to keep fseek from
-		 * `optimising' a backward seek, in case the user stomps on
-		 * the text.
-		 */
-		p++;		/* advance over it */
-		ret = (char *)fp->_p;
-		*lenp = len = p - fp->_p;
-		fp->_flags |= __SMOD;
-		fp->_r -= len;
-		fp->_p = p;
-		return (ret);
+	if (fgets(buffer, buflen+1, stream) == NULL)
+		return NULL;
+	removeCR(buffer);
+	*len = strlen(buffer);
+	while (*len == buflen && buffer[*len-1] != '\n') {
+		buffer = realloc(buffer, 2*buflen + 1);
+		if (fgets(buffer + buflen, buflen + 1, stream) == NULL)
+			return NULL;
+		removeCR(buffer);
+		*len += strlen(buffer + buflen);
+		buflen *= 2;
 	}
-
-	/*
-	 * We have to copy the current buffered data to the line buffer.
-	 * As a bonus, though, we can leave off the __SMOD.
-	 *
-	 * OPTIMISTIC is length that we (optimistically) expect will
-	 * accommodate the `rest' of the string, on each trip through the
-	 * loop below.
-	 */
-#define OPTIMISTIC 80
-
-	for (len = fp->_r, off = 0;; len += fp->_r) {
-		size_t diff;
-
-		/*
-		 * Make sure there is room for more bytes.  Copy data from
-		 * file buffer to line buffer, refill file and look for
-		 * newline.  The loop stops only when we find a newline.
-		 */
-		if (__slbexpand(fp, len + OPTIMISTIC))
-			goto error;
-		(void)memcpy((void *)(fp->_lb._base + off), (void *)fp->_p,
-		    len - off);
-		off = len;
-		if (__srefill(fp))
-			break;	/* EOF or error: return partial line */
-		if ((p = memchr((void *)fp->_p, '\n', fp->_r)) == NULL)
-			continue;
-
-		/* got it: finish up the line (like code above) */
-		p++;
-		diff = p - fp->_p;
-		len += diff;
-		if (__slbexpand(fp, len))
-			goto error;
-		(void)memcpy((void *)(fp->_lb._base + off), (void *)fp->_p,
-		    diff);
-		fp->_r -= diff;
-		fp->_p = p;
-		break;
-	}
-	*lenp = len;
-	return ((char *)fp->_lb._base);
-
-error:
-	*lenp = 0;		/* ??? */
-	return (NULL);		/* ??? */
+	return buffer;
 }

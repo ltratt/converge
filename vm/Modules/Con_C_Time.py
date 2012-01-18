32d45cec0c0a73751d21758523586c441fba1604
Laurence Tratt <laurie@tratt.net>
2012-01-18 23:21:54 +0000

    Add Time::current_mono.
    Time::current can become skewed by shifts in the system clock; current_mono
    is guaranteed to always monotonically increase, which is useful for timing.
    
    This patch also fixes a bug where microseconds in Time::current were
    incorrectly recorded as nanoseconds.

--- vm/Modules/Con_C_Time.py
+++ vm/Modules/Con_C_Time.py
@@ -19,6 +19,7 @@
 # IN THE SOFTWARE.
 
 
+from pypy.rlib import rarithmetic
 from pypy.rpython.lltypesystem import lltype, rffi
 from pypy.rpython.tool import rffi_platform as platform
 from pypy.translator.tool.cbuild import ExternalCompilationInfo
@@ -26,27 +27,33 @@ from Builtins import *
 
 
 
-eci     = ExternalCompilationInfo(includes=["sys/time.h"])
+eci             = ExternalCompilationInfo(includes=["sys/time.h"])
 
 class CConfig:
     _compilation_info_ = eci
     TIMEVAL            = platform.Struct("struct timeval", [("tv_sec", rffi.LONG), ("tv_usec", rffi.LONG)])
     TIMEZONE           = platform.Struct("struct timezone", [])
+    TIMESPEC           = platform.Struct("struct timespec", [("tv_sec", rffi.TIME_T), ("tv_nsec", rffi.LONG)])
+    CLOCK_MONOTONIC    = platform.DefinedConstantInteger("CLOCK_MONOTONIC")
 
-cconfig      = platform.configure(CConfig)
+cconfig         = platform.configure(CConfig)
 
-TIMEVAL      = cconfig["TIMEVAL"]
-TIMEVALP     = lltype.Ptr(TIMEVAL)
-TIMEZONE     = cconfig["TIMEZONE"]
-TIMEZONEP    = lltype.Ptr(TIMEZONE)
+TIMEVAL         = cconfig["TIMEVAL"]
+TIMEVALP        = lltype.Ptr(TIMEVAL)
+TIMEZONE        = cconfig["TIMEZONE"]
+TIMEZONEP       = lltype.Ptr(TIMEZONE)
+TIMESPEC        = cconfig["TIMESPEC"]
+TIMESPECP       = lltype.Ptr(TIMESPEC)
+CLOCK_MONOTONIC = cconfig["CLOCK_MONOTONIC"]
 
-gettimeofday = rffi.llexternal('gettimeofday', [TIMEVALP, TIMEZONEP], rffi.INT, compilation_info=eci)
+gettimeofday    = rffi.llexternal('gettimeofday', [TIMEVALP, TIMEZONEP], rffi.INT, compilation_info=eci)
+clock_gettime   = rffi.llexternal('clock_gettime', [rffi.INT, TIMESPECP], rffi.INT, compilation_info=eci)
     
 
 
 def init(vm):
     return new_c_con_module(vm, "C_Time", "C_Time", __file__, import_, \
-      ["current"])
+      ["current", "current_mono"])
 
 
 @con_object_proc
@@ -54,6 +61,7 @@ def import_(vm):
     (mod,),_ = vm.decode_args("O")
     
     new_c_con_func_for_mod(vm, "current", current, mod)
+    new_c_con_func_for_mod(vm, "current_mono", current_mono, mod)
 
     return vm.get_builtin(BUILTIN_NULL_OBJ)
 
@@ -68,4 +76,17 @@ def current(vm):
         sec = tp.c_tv_sec
         usec = tp.c_tv_usec
 
-    return Con_List(vm, [Con_Int(vm, sec), Con_Int(vm, usec)])
+    return Con_List(vm, [Con_Int(vm, sec), Con_Int(vm, usec * 1000)])
+
+
+@con_object_proc
+def current_mono(vm):
+    _,_ = vm.decode_args()
+
+    with lltype.scoped_alloc(TIMESPEC) as ts:
+        if clock_gettime(CLOCK_MONOTONIC, ts) != 0:
+            raise Exception("XXX")
+        sec = rarithmetic.r_int(ts.c_tv_sec)
+        nsec = rarithmetic.r_int(ts.c_tv_nsec)
+
+    return Con_List(vm, [Con_Int(vm, sec), Con_Int(vm, nsec)])
